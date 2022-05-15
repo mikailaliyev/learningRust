4e0d26555e6f029d305c666744037cb361691afa
mikailaliyev <mikail.aliyev@gmail.com>
2022-05-15 13:03:57 +0400

    comments

---
+++ 2_Comments.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x = 99;
+    //This is how we use format argument
+    /*It must be a string literal.
+    Well I just hope so, according to errors in my terminal 😁
+    */
+    //By the way comments are similar to C&JS
+    println!("My number is: {}", x);
+}
